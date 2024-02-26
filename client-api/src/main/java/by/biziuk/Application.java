bc11ba438d5386e915cdcc8e9244116ffffa0ecd
a_bizyuk <bizyuk.98@gmail.com>
2024-02-26 16:07:45 +0300

    Доделал

--- client-api/src/main/java/by/biziuk/Application.java
+++ client-api/src/main/java/by/biziuk/Application.java
@@ -2,13 +2,28 @@ package by.biziuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:8080")
 public class Application {
     
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
+    
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/post/create").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 }
